a484e32eca7a8e2d0da0250c5afb297a5efd7e4f
theVelopr <immortaldk@gmail.com>
2021-06-08 12:36:44 +0900

    Building a Spring Boot Web App Part 1.

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,79 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Compoenet
+ *  - telling that it's a Spring managed component
+ *  - telling that it's got two properties:
+ *      the author repo & book repo and these are final variables
+ *  - Initializing those inside the constructor which tells
+ *  the Spring Framework to when it constructs this bean that it has to inject
+ *  and instance of author repo and book repo.
+ *  - when it starts up, that's bean is configured by the Spring Framework,
+ *   and we are creating two author objects and two book objects and
+ *   using the repository methods to save them
+ * - underneath the covers, Spring Data JPA is utilizing Hibernate to save
+ * these two in memory H2.
+ *
+ */
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("Started in Bootstrap");
+
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
+
+        publisherRepository.save(publisher);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthor().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "5643132");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthor().add(rod);
+
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
+
+        System.out.println("Number of Books : " + bookRepository.count());
+        System.out.println("Publisher NUmber of Book: " + publisher.getBooks().size());
+    }
+}
