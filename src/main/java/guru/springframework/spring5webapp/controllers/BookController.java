4530b2431e3cdc3b68b013afd513b048a19522b4
theVelopr <immortaldk@gmail.com>
2021-06-15 10:06:21 +0900

    renamed & modified : renaming controller -> controllers & modified BookController.

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5webapp.controller;
+package guru.springframework.spring5webapp.controllers;
 
 import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
